34848e669351f1fe68f7debe84612cb26cc1696f
Nazar Shchepilov <nshchepilov@interkassa.com>
2016-12-27 00:24:27 +0200

    hmmm

--- app.go
+++ app.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	//"os"
-	//"github.com/nazarazzUA/cli"
-	//"github.com/nazarazzUA/commands"
-	//"golang.org/x/crypto/bcrypt"
-	//"fmt"
+
 	"github.com/nazarazzUA/cli"
 	"github.com/nazarazzUA/commands"
 	"os"
