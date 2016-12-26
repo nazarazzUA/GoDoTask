c58e68ad0eab25b89b346afb9f4b7d665b982756
Nazar Shchepilov <nshchepilov@interkassa.com>
2016-12-27 00:23:10 +0200

    remove comment section from code

--- app.go
+++ app.go
@@ -13,19 +13,6 @@ import (
 
 func main() {
 
-	//password := []byte("111111")
-	//
-	//// Hashing the password with the default cost of 10
-	//hashedPassword, err := bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//fmt.Println(string(hashedPassword))
-
-	//// Comparing the password with the hash
-	//err = bcrypt.CompareHashAndPassword(hashedPassword, password)
-	//fmt.Println(err) // nil means it is a match
-	//
 	cliApp := cli.NewCliApp();
 	commands.RegisterAllCommand(cliApp);
 	cliApp.ExecuteCommand(os.Args[1:]);
